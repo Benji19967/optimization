68aaf3a0882bd99b998ee02f8e78d57121fc5977
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-09-13 18:01:54 +0200

    Add code files

---
+++ src/gradient_descent.py
@@ -0,0 +1,16 @@
+def gradient_descent(f) -> tuple[float, float]:
+    """
+    Gradient descent for a function f: R -> R
+
+    Returns:
+        (argmin, min f)
+    """
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
