aeac2c624a98df59b70c608b48d4f1c737e96413
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-10-11 13:52:49 +0200

    Transform lp into equivalent lp but different form

--- src/linear_programming/lp01.py
+++ src/linear_programming/lp01.py
@@ -28,7 +28,11 @@ def lp_2():
     print(f"t*: {t.value:.1f}")
 
 
-def lp_3():
+def lp_3(verbose=True):
+    """
+    minimize norm_inf(2*x1 + 3*x2, -3*x1)
+    subject to |x1 - 2*x2 - 2| <= 1
+    """
     x1 = cp.Variable()
     x2 = cp.Variable()
     f = cp.hstack([2 * x1 + 3 * x2, -3 * x1])
@@ -37,7 +41,7 @@ def lp_3():
         cp.abs(x1 - 2 * x2 - 2) <= 1,
     ]
     prob = cp.Problem(obj, constraints)
-    prob.solve(verbose=True)
+    prob.solve(verbose=verbose)
 
     print(prob.status)
     print(f"f*: {prob.value:.2f}")
@@ -45,10 +49,69 @@ def lp_3():
     print(f"x2*: {x2.value:.2f}")
 
 
+def lp_4(verbose=True):
+    """
+    Equivalent to lp_3 but such that we have:
+        - All variables live in domain R+
+            - (make x1 = x1p - x1m, where x1p, x1m live in R+)
+        - no inequlity constraints exept variabl >= 0
+            - (turn '<=' to '==' via slack variables)
+    """
+    x1p = cp.Variable()
+    x1m = cp.Variable()
+    x2p = cp.Variable()
+    x2m = cp.Variable()
+    s1 = cp.Variable()
+    s2 = cp.Variable()
+    s3 = cp.Variable()
+    s4 = cp.Variable()
+    s5 = cp.Variable()
+    s6 = cp.Variable()
+    t = cp.Variable()
+
+    obj = cp.Minimize(t)
+    constraints = [
+        2 * (x1p - x1m) + 3 * (x2p - x2m) - t + s1 == 0,
+        -(2 * (x1p - x1m) + 3 * (x2p - x2m)) - t + s2 == 0,
+        -3 * (x1p - x1m) - t + s3 == 0,
+        -(-3 * (x1p - x1m)) - t + s4 == 0,
+        (x1p - x1m) - 2 * (x2p - x2m) - 2 - 1 + s5 == 0,
+        -((x1p - x1m) - 2 * (x2p - x2m) - 2) - 1 + s6 == 0,
+        x1p >= 0,
+        x1m >= 0,
+        x2p >= 0,
+        x2m >= 0,
+        s1 >= 0,
+        s2 >= 0,
+        s3 >= 0,
+        s4 >= 0,
+        s5 >= 0,
+        s6 >= 0,
+    ]
+    prob = cp.Problem(obj, constraints)
+    prob.solve(verbose=verbose)
+
+    print(prob.status)
+    print(f"f*: {prob.value:.2f}")
+    print(f"x1: {x1p.value - x1m.value:.2f}")
+    print(f"x2: {x2p.value - x2m.value:.2f}")
+
+    print(f"x1p: {x1p.value:.2f}")
+    print(f"x1m: {x1m.value:.2f}")
+    print(f"x2p: {x2p.value:.2f}")
+    print(f"x2m: {x2m.value:.2f}")
+    print(f"s1: {s1.value:.2f}")
+    print(f"s2: {s2.value:.2f}")
+    print(f"s3: {s3.value:.2f}")
+
+
 def main():
+    verbose = False
     # lp_1()
     # lp_2()
-    lp_3()
+
+    lp_3(verbose)
+    lp_4(verbose)
 
 
 if __name__ == "__main__":
