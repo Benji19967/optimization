4bfc2bb9f148b82241455640286bc23b9cc05cef
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-11-01 16:47:50 +0100

    feat: add scipy implementation of linear least squares

--- src/problems/p002.py
+++ src/problems/p002.py
@@ -9,11 +9,13 @@ Approach: use (linear) least squares
 """
 
 import cvxpy as cp
+import numpy as np
+from scipy.optimize import lsq_linear
 
 from src.plotting import plot_line, plot_line_sympy
 
 
-def least_squares(p1, p2):
+def least_squares_linear(p1, p2):
     """
     Minimize sum of residuals squared
     """
@@ -32,13 +34,28 @@ def least_squares(p1, p2):
     print(f"m*: {m.value}")
     print(f"b*: {b.value}")
 
-    return int(m.value), int(b.value)
+    return float(m.value), float(b.value)  # type: ignore
+
+
+def least_squares_linear_scipy(p1, p2):
+    """
+    Note: Ax = b vs y = mx + b, not the same b.
+    First is a constant vector, second a parameter.
+    """
+    res = lsq_linear(A=[[p1[0], 1], [p2[0], 1]], b=[p1[1], p2[1]])
+    m, b = res.x
+    return m, b
 
 
 def main():
     p1 = [0, 1]
     p2 = [1, 3]
-    m, b = least_squares(p1, p2)
+    m, b = least_squares_linear(p1, p2)
+    m1, b1 = least_squares_linear_scipy(p1, p2)
+    assert np.isclose(m, m1)
+    assert np.isclose(b, b1)
+
+    # plot_line(m, b, xlim=(-5, 5), ylim=(-5, 5))
     plot_line_sympy(m, b, xlim=(-5, 5), ylim=(-5, 5))
 
 
